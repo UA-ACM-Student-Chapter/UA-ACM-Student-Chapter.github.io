71817657f00495f8aaa4228bdc40aa5d1013596d
David <damccoy1@crimson.ua.edu>
2018-02-26 20:50:06 -0600

    updated method

--- js/client.js
+++ js/client.js
@@ -13,8 +13,32 @@
         dropDown.classList.remove("alert");
     });
 
+    function createCORSRequest(method, url) {
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+      
+          // Check if the XMLHttpRequest object has a "withCredentials" property.
+          // "withCredentials" only exists on XMLHTTPRequest2 objects.
+          xhr.open(method, url, true);
+      
+        } else if (typeof XDomainRequest != "undefined") {
+      
+          // Otherwise, check if XDomainRequest.
+          // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+          xhr = new XDomainRequest();
+          xhr.open(method, url);
+      
+        } else {
+      
+          // Otherwise, CORS is not supported by the browser.
+          xhr = null;
+      
+        }
+        return xhr;
+      }
 
-    var xhr = new XMLHttpRequest();
+
+    var xhr = createCORSRequest("GET", "https://ua-acm-web-payments.herokuapp.com/client_token");
     xhr.open("GET", "https://ua-acm-web-payments.herokuapp.com/client_token")
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
