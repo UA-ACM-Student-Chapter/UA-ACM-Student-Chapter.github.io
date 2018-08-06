6eaa73b38bbe1a0baedeff290ac8dc7cb37039dd
Jared Cleghorn <jaredrcleghorn@gmail.com>
2018-08-06 12:38:41 -0500

    Added the postcss task to the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,24 @@
 module.exports = function(grunt) {
-  // Do grunt-related things in here
+
+  // Project configuration.
+  grunt.initConfig({
+    postcss: {
+      options: {
+        processors: [
+          require('cssnano')() // minify the result
+        ]
+      },
+      dist: {
+        src: 'css/styles.min.css',
+        dest: 'css/styles.min.css'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "postcss" task.
+  grunt.loadNpmTasks('grunt-postcss');
+
+  // Default task(s).
+  grunt.registerTask('default', ['postcss']);
+
 };
