3877dfce6005ac4e43a221e75013d0e8b1fbc334
Jared Cleghorn <jaredrcleghorn@gmail.com>
2018-08-06 00:46:06 -0500

    Renamed some stuff, mostly

--- js/noJs.js
+++ js/noJs.js
@@ -1,14 +1,14 @@
 //
-// noJsMessages.js
+// noJs.js
 // UA-ACM-Student-Chapter.github.io
 //
 // Created by Jared Cleghorn on 7/21/18.
 // Copyright © 2018 UA ACM. All right reserved.
 
 // Get elements that should not be displayed if JavaScript is enabled.
-var noJsMessages = document.getElementsByClassName('no-js-message');
+var noJsElements = document.getElementsByClassName('no-js');
 
 // Set their 'display' property to 'none'.
-for (let noJsMessage of noJsMessages) {
-  noJsMessage.className = noJsMessage.className + ' display-none';
+for (let noJsElement of noJsElements) {
+  noJsElement.className = 'js';
 }
