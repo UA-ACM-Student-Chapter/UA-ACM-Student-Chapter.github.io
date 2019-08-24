4e3984ea0c1111794521d878deb446316d232bfd
Ethan Mines <elmines325@gmail.com>
2019-08-24 14:24:59 -0500

    Fix calendar with new API key

--- js/config-vars.js
+++ js/config-vars.js
@@ -1,5 +1,5 @@
 var UTIL_INSTANCE_NAME = "ua-acm-web-util";
 var PAYMENTS_INSTANCE_NAME = "ua-acm-web-payments";
 var CALENDAR_NAME = "vfgqdivlam7s8bai2q63c9bot8@group.calendar.google.com";
-var GOOGLE_CAL_API_KEY = 'AIzaSyBTHWumQn9L2u6mb0dftRSEWUHmlaX-xEc';
-const NUM_UPCOMING_EVENTS_TO_DISPLAY = 9;
+var GOOGLE_CAL_API_KEY = 'AIzaSyChLJnfGP_jeIkdPt432rdMQchZi3bn7t0';
+const NUM_UPCOMING_EVENTS_TO_DISPLAY = 3;
