9437241decaaa9f77f4711a6e4d0e49954b45741
Ethan Mines <elmines325@gmail.com>
2019-08-25 08:45:07 -0500

    Reduce number of text boxes

--- js/config-vars.js
+++ js/config-vars.js
@@ -2,4 +2,4 @@ var UTIL_INSTANCE_NAME = "ua-acm-web-util";
 var PAYMENTS_INSTANCE_NAME = "ua-acm-web-payments";
 var CALENDAR_NAME = "vfgqdivlam7s8bai2q63c9bot8@group.calendar.google.com";
 var GOOGLE_CAL_API_KEY = 'AIzaSyChLJnfGP_jeIkdPt432rdMQchZi3bn7t0';
-const NUM_UPCOMING_EVENTS_TO_DISPLAY = 3;
+const NUM_UPCOMING_EVENTS_TO_DISPLAY = 6;
